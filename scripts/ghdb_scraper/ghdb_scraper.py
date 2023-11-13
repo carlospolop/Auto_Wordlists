dc3e45dd614d139eeb99b03c0d6e05d0ef8e257e
Axylum <cedric.legrand@gmail.com>
2023-11-13 20:04:29 +0100

    Minor Python source code enhancement

--- scripts/ghdb_scraper/ghdb_scraper.py
+++ scripts/ghdb_scraper/ghdb_scraper.py
@@ -53,23 +53,28 @@ headers = {
 }
 
 
-
 def check_domain(data):
     return validators.domain(data) is True
+
 
 def get_dork_description(url_path):
     url = f"https://www.exploit-db.com{url_path}"
     response = requests.get(url, headers=headers, verify=True)
     soup = BeautifulSoup(response.content, "html.parser")
     meta = soup.find_all('meta')
-    for tag in meta:
-        if 'name' in tag.attrs.keys() and tag.attrs['name'].strip().lower() == 'description':
-            return tag.attrs['content']
-    return ""
+    return next(
+        (
+            tag.attrs['content']
+            for tag in meta
+            if 'name' in tag.attrs.keys()
+            and tag.attrs['name'].strip().lower() == 'description'
+        ),
+        "",
+    )
 
 
 def FIX_DORK(dork):
-    #If "-site:gomain.com" remove it
+    # If "-site:gomain.com" remove it
     for match in re.findall(r"((-site:|-inurl|site|inurl)[\"']?([\w\-\.]+)[\"']?)", dork):
         if check_domain(match[2]):
             dork = dork.replace(match[0], "")
@@ -130,28 +135,30 @@ def retrieve_google_dorks(json_path, save_individual_categories):
             # Provide some category metrics.
             print(f"[*] Category {key} ('{value['category_name']}') has {len(value['dorks'])} dorks")
 
-            #dork_file_name = value["category_name"].lower().replace(" ", "_")
-            #full_dork_file_name = f"dorks/{dork_file_name}.dorks"
+            # dork_file_name = value["category_name"].lower().replace(" ", "_")
+            # full_dork_file_name = f"dorks/{dork_file_name}.dorks"
             DORKS_TYPE = value["category_name"]
             MYDORKS[DORKS_TYPE] = []
 
-            #print(f"[*] Writing dork category '{value['category_name']}' to file: {full_dork_file_name}")
+            # print(f"[*] Writing dork category '{value['category_name']}' to file: {full_dork_file_name}")
 
-            #with open(f"{full_dork_file_name}", "w", encoding="utf-8") as fh:
+            # with open(f"{full_dork_file_name}", "w", encoding="utf-8") as fh:
             for dork in value["dorks"]:
                 # Extract dork from <a href> using BeautifulSoup.
                 # "<a href=\"/ghdb/5052\">inurl:_cpanel/forgotpwd</a>"
                 soup = BeautifulSoup(dork["url_title"], "html.parser")
                 extracted_dork = soup.find("a").contents[0]
-                description = get_dork_description(soup.find("a")["href"]).replace("\n\n","\n").replace("\n\n","\n").replace("\n\n","\n").replace("\n\n","\n").strip()
+                description = get_dork_description(soup.find("a")["href"]).replace("\n\n", "\n").replace("\n\n",
+                                                                                                         "\n").replace(
+                    "\n\n", "\n").replace("\n\n", "\n").strip()
                 DORK = FIX_DORK(extracted_dork)
                 if DORK != "" and len(DORK) > 5:
                     if len(description) == 0:
                         print(f"NULL DESCRIPTION FOR {extracted_dork}")
                     MYDORKS[DORKS_TYPE].append({"dork": extracted_dork, "description": description})
-                #fh.write(f"{extracted_dork}\n")
+                # fh.write(f"{extracted_dork}\n")
 
-    #SAVE MYDORKS INSIDE A FILE
+    # SAVE MYDORKS INSIDE A FILE
     with open(json_path, "w", encoding="utf-8") as json_file:
         json.dump(MYDORKS, json_file)
 
@@ -162,13 +169,10 @@ def get_timestamp():
     """Retrieve a pre-formated datetimestamp."""
 
     now = time.localtime()
-    timestamp = time.strftime("%Y%m%d_%H%M%S", now)
-
-    return timestamp
+    return time.strftime("%Y%m%d_%H%M%S", now)
 
 
 if __name__ == "__main__":
-
     categories = {
         1: "Footholds",
         2: "File Containing Usernames",
@@ -201,7 +205,7 @@ if __name__ == "__main__":
         "-i",
         dest="save_individual_categories",
         action="store_true",
-        default=True, #Use always to generate the dorks json
+        default=True,  # Use always to generate the dorks json
         help="Write all the individual dork type files based off the category.",
     )
 
@@ -212,7 +216,6 @@ if __name__ == "__main__":
         default="ghdb.json",
         help="Path to the .json file to save the info",
     )
-
 
     args = parser.parse_args()
 
